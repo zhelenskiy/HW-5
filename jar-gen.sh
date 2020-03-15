45bf51e8584aa2bcf0afab53084a86b77e959f66
zhelenskiy <zhelenskiy2000@yandex.ru>
2020-03-15 23:48:22 +0300

    Tests updated

--- jar-gen.sh
+++ jar-gen.sh
@@ -13,6 +13,6 @@ jar xf ../artifacts/info.kgeorgiy.java.advanced.implementor.jar \
         info/kgeorgiy/java/advanced/implementor/ImplerException.class
 
 mkdir ../jar-files 2> /dev/null
-jar cfm ../jar-files/Implementor.jar Manifest.txt \
+jar cvfe ../jar-files/Implementor.jar info/kgeorgiy/java/advanced/implementor/Impler.class \
      production/ru.ifmo.rain.zhelenskiy.implementor/ru/ifmo/rain/zhelenskiy/implementor/*.class \
      info/kgeorgiy/java/advanced/implementor/*.class
